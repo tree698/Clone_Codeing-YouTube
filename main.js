e388953b665eead196176b9c20e7eff278c44f5f
Chanwoo <tree698@gmail.com>
2022-09-15 17:31:19 +0900

    additional add JavaScript: header + video; bars, background, sideBar

--- main.js
+++ main.js
@@ -37,32 +37,38 @@ function removeSubtitle(element, targetElement) {
 }
 
 // header: sidebar
+const headBars = document.querySelector('button.bars.head');
+const sideBars = document.querySelector('button.bars.sideBar');
 const logoSideBar = document.querySelector('.logo__sideBar');
-const bars = document.querySelector('.bars');
-const barsSideBar = document.querySelector('.bars.sideBar');
-const header = document.querySelector('.header');
-const contents = document.querySelector('.contents');
+const backgroundBlack = document.querySelector('.background-black');
 
-bars.addEventListener('click', () => {
-  logoSideBar.classList.remove('hide');
-  header.style.opacity = '0.7';
-  contents.style.opacity = '0.7';
+headBars.addEventListener('click', () => {
+  logoSideBar.style.left = '0px';
+
+  headBars.classList.toggle('buttonBars__animation');
+  setTimeout(() => {
+    headBars.classList.toggle('buttonBars__animation');
+  }, 1001);
+  backgroundBlack.classList.toggle('hide');
 });
 
-barsSideBar.addEventListener('click', () => {
-  logoSideBar.classList.add('hide');
-  header.style.opacity = '1';
-  contents.style.opacity = '1';
+sideBars.addEventListener('click', () => {
+  logoSideBar.style.left = '-360px';
+  sideBars.classList.toggle('buttonBars__animation');
+  setTimeout(() => {
+    sideBars.classList.toggle('buttonBars__animation');
+  }, 1001);
+  backgroundBlack.classList.toggle('hide');
 });
 
 // Section: Video => subtitle
-const infoRightThumbsup = document.querySelector('.info__right.thumbsup');
-const infoRightThumbsdown = document.querySelector('.info__right.thumbsdown');
-const infoRightshare = document.querySelector('.info__right.share');
-const infoRightofflineSave = document.querySelector(
-  '.info__right.offline-save'
+const infoMiddleThumbsup = document.querySelector('.info__middle.thumbsup');
+const infoMiddleThumbsdown = document.querySelector('.info__middle.thumbsdown');
+const infoMiddleshare = document.querySelector('.info__middle.share');
+const infoMiddleofflineSave = document.querySelector(
+  '.info__middle.offline-save'
 );
-const infoRightsave = document.querySelector('.info__right.save');
+const infoMiddlesave = document.querySelector('.info__middle.save');
 
 const thumbsupSubtitle = document.querySelector('.thumbsup__subtitle');
 const thumbsdownSubtitle = document.querySelector('.thumbsdown__subtitle');
@@ -70,17 +76,17 @@ const shareSubtitle = document.querySelector('.share__subtitle');
 const offlieSaveSubtitle = document.querySelector('.offline-save__subtitle');
 const saveSubtitle = document.querySelector('.save__subtitle');
 
-displaySubtitle(infoRightThumbsup, thumbsupSubtitle);
-displaySubtitle(infoRightThumbsdown, thumbsdownSubtitle);
-displaySubtitle(infoRightshare, shareSubtitle);
-displaySubtitle(infoRightofflineSave, offlieSaveSubtitle);
-displaySubtitle(infoRightsave, saveSubtitle);
+displaySubtitle(infoMiddleThumbsup, thumbsupSubtitle);
+displaySubtitle(infoMiddleThumbsdown, thumbsdownSubtitle);
+displaySubtitle(infoMiddleshare, shareSubtitle);
+displaySubtitle(infoMiddleofflineSave, offlieSaveSubtitle);
+displaySubtitle(infoMiddlesave, saveSubtitle);
 
-removeSubtitle(infoRightThumbsup, thumbsupSubtitle);
-removeSubtitle(infoRightThumbsdown, thumbsdownSubtitle);
-removeSubtitle(infoRightshare, shareSubtitle);
-removeSubtitle(infoRightofflineSave, offlieSaveSubtitle);
-removeSubtitle(infoRightsave, saveSubtitle);
+removeSubtitle(infoMiddleThumbsup, thumbsupSubtitle);
+removeSubtitle(infoMiddleThumbsdown, thumbsdownSubtitle);
+removeSubtitle(infoMiddleshare, shareSubtitle);
+removeSubtitle(infoMiddleofflineSave, offlieSaveSubtitle);
+removeSubtitle(infoMiddlesave, saveSubtitle);
 
 // Section: Video => ellipsis
 const ellipsisBtn = document.querySelector('.ellipsisBtn');
